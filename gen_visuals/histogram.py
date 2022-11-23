896bef90fd64a3f0d3261784f74f018ece09d9ff
Noah Shinn <noahshinn024@gmail.com>
2022-11-23 14:32:43 -0500

    .

--- gen_visuals/histogram.py
+++ gen_visuals/histogram.py
@@ -2,24 +2,40 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-NBINS = 20
+from typing import List
 
-with open('./data-nacs.json') as f:
+NBINS = 20
+BIN_WIDTH = 0.015
+
+with open('../../scratch/filtered-005-02.json') as f:
     data_orig = json.load(f)
     orig = np.abs(np.asarray(data_orig['nacs'])).mean(axis=(1, 2))
-    orig_energies = np.asarray(data_orig['energy_differences']) * 27.2114
+    orig_energies = np.asarray(data_orig['e_diffs']) * 27.2114
 
 # with open('./out.json') as f:
     # data_sampled = json.load(f)
     # sampled = np.stack([np.abs(np.asarray(sample['nacs'])).mean() for sample in data_sampled], axis=0)
     # sampled = np.stack([np.asarray(sample['e_diff']) for sample in data_sampled], axis=0)
 
+def get_assigned_pts(arr: np.ndarray, bins: np.ndarray) -> List[np.ndarray]:
+    res = [[] for _ in range(NBINS)]
+    for pt in arr:
+        for i, b in enumerate(bins):
+            if pt >= b and pt < b + BIN_WIDTH:
+                res[i] += [pt]
+    res = [np.asarray(res[i]) for i in range(NBINS)]
+    return res
+
 fig, axs = plt.subplots()
-axs.hist(orig_energies, range=(-1.0, 0.0))
-axs.set_xlabel('energy differences (eVs)')
+mean = orig.mean()
+std = orig.std()
+axs.hist(orig, range=(mean - std * 3, mean + std * 3))
+# n, bins, patches = axs.hist(orig, bins=NBINS, range=(-0.2, -0.05))
+axs.set_xlabel('avg nac elem value')
 axs.set_ylabel('count')
-# plt.show()
-plt.savefig('e_diff.png')
+axs.set_title(f'Histogram of avg nac elem values mean={mean.round(2)}, std={std.round(2)}')
+plt.show()
+plt.savefig('avg_nac_elem_value.png')
 import sys
 sys.exit()
 
--- gen_visuals/scatter.py
+++ gen_visuals/scatter.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 
-with open('./data-nacs.json', 'r') as f:
+with open('../../scratch/filtered-005-02.json', 'r') as f:
     data = json.load(f)
-    e_diffs = np.asarray(data['energy_differences']) * 27.2114
+    e_diffs = np.asarray(data['e_diffs']) * 27.2114
     avg_nac_elems = np.abs(np.asarray(data['nacs'])).mean(axis=(1, 2))
 
 # xy = np.vstack([e_diffs, avg_nac_elems])
@@ -13,6 +13,9 @@ with open('./data-nacs.json', 'r') as f:
     
 # plt.scatter(e_diffs, avg_nac_elems, c=z, s=100)
 plt.scatter(e_diffs, avg_nac_elems)
-plt.xlim(-0.3, 0.0)
+plt.xlabel('energy difference (eVs)')
+plt.ylabel('avg nac elem value')
+# plt.xlim(-0.2, 0.0)
+# plt.ylim(0.0, 25.0)
 # plt.show()
-plt.savefig('e_diff_vs_avg_nac.png')
+plt.savefig('e_diff_vs_avg_nac_02_005_full.png')
