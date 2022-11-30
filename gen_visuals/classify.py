6a1926fa8d371e6513ad25e55cdaec24304b3485
Noah Shinn <noahshinn024@gmail.com>
2022-11-30 17:36:30 -0500

    all

--- gen_visuals/classify.py
+++ gen_visuals/classify.py
@@ -3,50 +3,70 @@ import json
 """
 BIN KEY:
     0: zero
-    1: non-zero
-    2: infinity
+    1: [-0.2, -0.1625)
+    2: [-0.1625, -0.125)
+    3: [-0.125, -0.0875)
+    4: [-0.0875, -0.05)
+    5: infinity
 
 """
 
-# with open('./data-nacs.json', 'r') as f:
-    # data = json.load(f)
-    # l = len(data['atom_types'])
+NBINS = 6
+
+with open('./data-nacs.json', 'r') as f:
+    data = json.load(f)
+    l = len(data['atom_types'])
     
-    # bins = []
-    # for i in range(l):
-        # mag = abs(data['energy_differences'][i] * 27.2107)
-        # if mag > 0.2:
-            # bins += [0]
-        # elif mag > 0.05:
-            # bins += [1]
-        # else:
-            # bins += [2]
+    bins = []
+    for i in range(l):
+        mag = abs(data['energy_differences'][i] * 27.2107)
+        if mag > 0.2:
+            bins += [0]
+        elif mag > 0.1625:
+            bins += [1]
+        elif mag > 0.125:
+            bins += [2]
+        elif mag > 0.0875:
+            bins += [3]
+        elif mag > 0.05:
+            bins += [4]
+        else:
+            bins += [5]
 
-    # new_data = {}
-    # new_data['species'] = data['atom_types']
-    # new_data['coords'] = data['coords']
-    # new_data['nacs'] = data['nacs']
-    # new_data['norms'] = data['norms']
-    # new_data['e_diffs'] = data['energy_differences']
-    # new_data['bin'] = bins
+    new_data = {}
+    new_data['species'] = data['atom_types']
+    new_data['coords'] = data['coords']
+    new_data['nacs'] = data['nacs']
+    new_data['norms'] = data['norms']
+    new_data['e_diffs'] = data['energy_differences']
+    new_data['bin'] = bins
 
-    # with open('nacs-3-bins.json', 'w') as f:
-        # json.dump(new_data, f)
-        # print(f'dumped {l} structures to file')
+    with open(f'nacs-{NBINS}-bins.json', 'w') as f:
+        json.dump(new_data, f)
+        print(f'dumped {l} structures to file')
 
-with open('./nacs-3-bins.json', 'r') as f:
+with open(f'./nacs-{NBINS}-bins.json', 'r') as f:
     data = json.load(f)
     nstructures = len(data['species'])
     zero = 0
-    non_zero = 0
+    one = 0
+    two = 0
+    three = 0
+    four = 0
     infinity = 0
     for i in range(nstructures):
         b = data['bin'][i]
         if b == 0:
             zero += 1
         elif b == 1:
-            non_zero += 1
+            one += 1
         elif b == 2:
+            two += 1
+        elif b == 3:
+            three += 1
+        elif b == 4:
+            four += 1
+        elif b == 5:
             infinity += 1
         else:
             import sys
@@ -55,7 +75,10 @@ with open('./nacs-3-bins.json', 'r') as f:
 
     print(f'BINS:')
     print(f'zero: {zero}, {(zero / nstructures):.4f}')
-    print(f'non-zero: {non_zero}, {(non_zero / nstructures):.4f}')
+    print(f'one: {one}, {(one / nstructures):.4f}')
+    print(f'two: {two}, {(two / nstructures):.4f}')
+    print(f'three: {three}, {(three / nstructures):.4f}')
+    print(f'four: {four}, {(four / nstructures):.4f}')
     print(f'infinity: {infinity}, {(infinity / nstructures):.4f}')
 
 
